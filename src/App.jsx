fc1a125f4cc8061ef922a52f2cdceaacc6f7aa5b
jfrisina <jaki.frisina@gmail.com>
2024-07-12 17:03:50 -0700

    create add, toggle, and delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,58 @@
-// import 
 import { useReducer, useState } from 'react'
 import './App.css'
-import Data from './components/data'
+import Todo from './Todo'
 
-// reducer function
-function reducer(state, action) {
-  // code goes here
-
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
 }
 
-// app code
+function reducer(todos, action) { // takes in current state, and thentakes an action from the dispactch function from below
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete }
+        }
+        return todo
+      })
+    case ACTIONS.DELETE_TODO: 
+      return todos.filter(todo => todo.id !== action.payload.id)
+
+    default: 
+    return todos
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false}
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, dispatch] = useReducer(reducer, []) // calling reducer function above. 
+  const [name, setName] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name} })
+      setName('')
+  }
+
+  console.log(todos)
 
   return (
     <>
-      <h1>To Do List</h1>        
-      <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      <Data />
+      <form onSubmit={handleSubmit}> 
+        <input type="text" value={name} onChange={event => setName(event.target.value)} />
+      </form>
+      {todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch}/>
+      })}
     </>
   )
 }
 
 export default App
-
